35042067e911d5bfedef86d510004c5623d20902
timkim <timk@adobe.com>
2024-01-25 23:21:45 +0000

    Deploying to gh-pages from @ AdobeDocs/photoshop-api@29b49092fa3e2e548e34cc557126ae7d092b6ae1 🚀

---
+++ component---src-pages-demo-md-780c6510f20892ddbe78.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_site_documentation_template=self.webpackChunkdev_site_documentation_template||[]).push([[576],{6942:function(t,n,e){e.r(n),e.d(n,{_frontmatter:function(){return s},default:function(){return p}});var o=e(87462),u=e(45987),c=(e(35776),e(3905)),r=e(91515);const m=["components"],s={},a={_frontmatter:s},i=r.Z;function p(t){let{components:n}=t,e=(0,u.Z)(t,m);return(0,c.mdx)(i,(0,o.Z)({},a,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-demo-md-780c6510f20892ddbe78.js.map
